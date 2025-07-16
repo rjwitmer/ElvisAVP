15fd5ec8ce16cc3854785769b45d9ce332a7df3b
Bob Witmer <bwitmer@golden.net>
2025-07-16 10:07:12 -0400

    Initial Commit

---
+++ ElvisAVP/ElvisAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisAVPApp.swift
+//  ElvisAVP
+//
+//  Created by Bob Witmer on 2025-07-16.
+//
+
+import SwiftUI
+
+@main
+struct ElvisAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
